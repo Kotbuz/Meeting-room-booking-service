4842c77a9d9c808122b1283ea649671d991c0a2a
Kotbuz <wasdtv03@gmail.com>
2026-07-21 23:00:51 +0700

    Добавил конфигурацию для подключения к базе данных PostgreSQL и настройки приложения через Pydantic.

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str
+    postgres_port: int
+
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    debug: bool = False
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+    @property
+    def database_url(self) -> str:
+        return f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+
+
+settings = Settings()
--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
-
+from app.core.config import settings
 from sqlalchemy.orm import DeclarativeBase
 
-engine = create_async_engine("sqlite+aiosqlite:///bookings.db")
+engine = create_async_engine(settings.database_url, echo=settings.debug,)
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
 
