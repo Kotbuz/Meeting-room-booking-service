3705316c603da4d3af8c58a8701b2e79cfa7b0e7
Kotbuz <wasdtv03@gmail.com>
2026-07-26 14:57:36 +0700

    рабочий CRUD Timeslots

--- app/services/timeslot_service.py
+++ app/services/timeslot_service.py
@@ -4,6 +4,7 @@ from fastapi import HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 
+from app.repositories import room_repository
 import app.repositories.timeslot_repository as repository
 from app.schemas.timeslots import TimeSlotCreateSchema, TimeSlotUpdateSchema
 
@@ -24,8 +25,21 @@ def normalize_datetime(dt: datetime | None) -> datetime | None:
     return dt
 
 
+def validate_start_time(start_time: datetime):
+    if normalize_datetime(start_time) <= datetime.now():
+        raise HTTPException(
+            status_code=400,
+            detail="Start time cannot be in the past",
+        )
+
+
 async def create_timeslot(session: AsyncSession, data: TimeSlotCreateSchema):
 
+    room = await room_repository.get_room_by_id(session, data.room_id)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
+    validate_start_time(data.start_time)
+    validate_time_interval(data.start_time, data.end_time)
     existing_timeslot = await repository.get_intersecting_timeslots(
         session,
         data.room_id,
@@ -35,9 +49,8 @@ async def create_timeslot(session: AsyncSession, data: TimeSlotCreateSchema):
     if existing_timeslot:
         raise HTTPException(
             status_code=400,
-            detail="TimeSlot with this time already exists in this room",
+            detail="TimeSlot overlaps with another timeslot",
         )
-    validate_time_interval(data.start_time, data.end_time)
     new_timeslot = await repository.create_timeslot(
         session,
         data.room_id,
@@ -58,6 +71,9 @@ async def get_timeslot_by_room_and_time(
     room_id: int,
     time: datetime,
 ):
+    room = await room_repository.get_room_by_id(session, room_id)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
     return await repository.get_timeslot_by_room_and_time(
         session, room_id, normalize_datetime(time)
     )
@@ -70,6 +86,9 @@ async def get_intersecting_timeslots(
     end_time: datetime,
 ):
 
+    room = await room_repository.get_room_by_id(session, room_id)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
     validate_time_interval(start_time, end_time)
     return await repository.get_intersecting_timeslots(
         session, room_id, normalize_datetime(start_time), normalize_datetime(end_time)
@@ -83,10 +102,14 @@ async def get_all_timeslots(session: AsyncSession):
 async def update_timeslot(
     session: AsyncSession, timeslot_id: int, data: TimeSlotCreateSchema
 ):
+    room = await room_repository.get_room_by_id(session, data.room_id)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
+    validate_start_time(data.start_time)
     validate_time_interval(data.start_time, data.end_time)
     timeslot = await repository.get_timeslot_by_id(session, timeslot_id)
     if not timeslot:
-        raise HTTPException(status_code=404, detail="Timeslot does not exists")
+        raise HTTPException(status_code=404, detail="Timeslot does not exist")
 
     intersections = await repository.get_intersecting_timeslots(
         session,
@@ -115,22 +138,28 @@ async def update_timeslot(
 async def partial_update_timeslot(
     session: AsyncSession, timeslot_id: int, data: TimeSlotUpdateSchema
 ):
-    validate_time_interval(data.start_time, data.end_time)
+
     timeslot = await repository.get_timeslot_by_id(session, timeslot_id)
     if not timeslot:
-        raise HTTPException(status_code=404, detail="Timeslot does not exists")
+        raise HTTPException(status_code=404, detail="Timeslot does not exist")
+
     new_room_id = data.room_id if data.room_id is not None else timeslot.room_id
-    new_start_time = (
-        data.start_time if data.start_time is not None else timeslot.start_time
-    )
-    new_end_time = data.end_time if data.end_time is not None else timeslot.end_time
-    validate_time_interval(new_start_time, new_end_time)
+    new_start = data.start_time if data.start_time is not None else timeslot.start_time
+    new_end = data.end_time if data.end_time is not None else timeslot.end_time
+
+    validate_start_time(new_start)
+
+    room = await room_repository.get_room_by_id(session, new_room_id)
+    if not room:
+        raise HTTPException(status_code=404, detail="Room not found")
+
+    validate_time_interval(new_start, new_end)
 
     intersections = await repository.get_intersecting_timeslots(
         session,
         new_room_id,
-        normalize_datetime(new_start_time),
-        normalize_datetime(new_end_time),
+        normalize_datetime(new_start),
+        normalize_datetime(new_end),
         exclude_id=timeslot.id,
     )
 
@@ -154,7 +183,7 @@ async def delete_timeslot(session: AsyncSession, timeslot_id: int):
     timeslot = await repository.get_timeslot_by_id(session, timeslot_id)
 
     if not timeslot:
-        raise HTTPException(status_code=404, detail="Timeslot does not exists")
+        raise HTTPException(status_code=404, detail="Timeslot does not exist")
 
     await repository.delete_timeslot(session, timeslot)
     await repository.save(session)
